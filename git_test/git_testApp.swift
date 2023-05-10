010ad74ffe365c3f3ef541e5ceb6e12a7233c4d9
Caleb <jjy050708@gmail.com>
2023-05-10 10:16:24 +0900

    Initial Commit

---
+++ git_test/git_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_testApp.swift
+//  git_test
+//
+//  Created by 정주영 on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct git_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
